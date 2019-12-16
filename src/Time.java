2bf65e91aa888bdbc9a630dbfbcdf46df8340aa9
josue-07 <josuelima875@gmail.com>
2019-12-16 15:16:55 -0300

    Criação do construtor Time

--- src/Time.java
+++ src/Time.java
@@ -9,5 +9,8 @@ public class Time {
 	public Time(String nome, String categoria) {
 		this.nome = nome;
 		this.categoria = categoria;
+		this.anoCriacao = 0;
+		this.quantSocios = 0;
+		this.historico = "indefinido";
 	}
 }
