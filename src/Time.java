df46d1eec5fdd47dcc7678c7e5d7a5e18f51eade
arinelson <arinelson_hermano@hotmail.com>
2019-12-16 14:50:58 -0300

    criação da classe Time e seus atributos

---
+++ src/Time.java
@@ -0,0 +1,13 @@
+
+public class Time {
+	private String nome;
+	private String categoria;
+	private int anoCriacao;
+	private int quantSocios;
+	private String historico;
+	
+	public Time(String nome, String categoria) {
+		this.nome = nome;
+		this.categoria = categoria;
+	}
+}
