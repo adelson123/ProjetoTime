4577b4417325652c363af42ea1633f46ee4b37a6
josue-07 <josuelima875@gmail.com>
2019-12-16 15:34:33 -0300

    Criação do método calculaIdade

--- src/Time.java
+++ src/Time.java
@@ -5,12 +5,21 @@ public class Time {
 	private int anoCriacao;
 	private int quantSocios;
 	private String historico;
+	private int idadeTime;
+	
 	
 	public Time(String nome, String categoria) {
 		this.nome = nome;
 		this.categoria = categoria;
-		this.anoCriacao = 0;
+		this.anoCriacao = 2000;
 		this.quantSocios = 0;
 		this.historico = "indefinido";
+		this.idadeTime = 2019;
+			}
+	
+	public int calculaIdade() {
+		this.idadeTime = this.idadeTime - anoCriacao;
+			return idadeTime;
 	}
+
 }
